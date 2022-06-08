e90c48a6b2213f9c4ed863c6a0dc28962f38b5fe
LargeSloth <skijumper2010@gmail.com>
2022-06-08 10:41:29 -0500

    Template for 16x16 square grid

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const container = document.getElementById('canvas')
 function makeGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < (rows * cols); i++) {
+    for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (i + 1);
+        cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item"
     };
 };
